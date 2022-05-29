88865faa0d49f5f30179bad7de56d08ccdd472bf
Gabriel Magalhães <magalhaes.gabrielf@gmail.com>
2022-05-29 18:46:24 -0300

    add test to ensure bad request response for invalid json

---
+++ api/user/controller_test.go
@@ -0,0 +1,31 @@
+package user
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBadRequestWhenPostMethod(t *testing.T) {
+	expectBody := "{\"code\":\"bad_request\",\"message\":\"Bad Request\",\"data\":{}}\n"
+
+	e := echo.New()
+
+	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader("invalid-json"))
+	rec := httptest.NewRecorder()
+
+	c := e.NewContext(req, rec)
+
+	c.SetPath("/v1/users")
+
+	h := &Handler{}
+
+	if assert.NoError(t, h.Post(c)) {
+		assert.Equal(t, http.StatusBadRequest, rec.Code)
+		assert.Equal(t, expectBody, rec.Body.String())
+	}
+}
