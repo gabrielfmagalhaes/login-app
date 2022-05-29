cdcfeef07e4e0463147ff4fddca2e869a5b4cee3
Gabriel Magalhães <magalhaes.gabrielf@gmail.com>
2022-05-29 19:02:16 -0300

    add response struct to abstract structs

--- api/common/error_controller_response.go
+++ api/common/error_controller_response.go
@@ -2,21 +2,9 @@ package common
 
 import "net/http"
 
-type responseCode string
-
-const (
-	errBadRequest responseCode = "bad_request"
-)
-
-type controllerResponse struct {
-	Code    responseCode `json:"code"`
-	Message string       `json:"message"`
-	Data    interface{}  `json:"data"`
-}
-
-func NewBadRequestResponse() (int, controllerResponse) {
-	return http.StatusBadRequest, controllerResponse{
-		errBadRequest,
+func NewBadRequestResponse() (int, ControllerResponse) {
+	return http.StatusBadRequest, ControllerResponse{
+		"bad_request",
 		"Bad Request",
 		map[string]interface{}{},
 	}
