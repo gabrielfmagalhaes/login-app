a47d595270a1754d4fc4b8bd83d2c510b5a2dda4
Gabriel Magalhães <magalhaes.gabrielf@gmail.com>
2022-06-12 22:14:58 -0300

    refactor: change mapper to adapter structure

---
+++ internal/api/user/adapter/user.go
@@ -0,0 +1,14 @@
+package adapter
+
+import (
+	"login-app/internal/api/user/dto"
+	"login-app/internal/core/domain"
+)
+
+func ToDomain(dto dto.CreateUserRequest) *domain.User {
+	return &domain.User{
+		Name:     dto.Name,
+		Email:    dto.Email,
+		Password: dto.Password,
+	}
+}
