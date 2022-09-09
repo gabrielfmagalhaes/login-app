6ae1d00f068e3392fa40ac92b313a4aae1fde583
Gabriel Magalhães <magalhaes.gabrielf@gmail.com>
2022-09-08 21:08:15 -0300

    remove adapter ToDomain

--- internal/core/domain/user.go
+++ internal/core/domain/user.go
@@ -1,8 +1,18 @@
 package domain
 
+import "login-app/internal/api/web/user/dto"
+
 type User struct {
 	ID       string
 	Name     string
 	Email    string
 	Password string
+}
+
+func ToDomain(dto *dto.CreateUserRequest) *User {
+	return &User{
+		Name:     dto.Name,
+		Email:    dto.Email,
+		Password: dto.Password,
+	}
 }
