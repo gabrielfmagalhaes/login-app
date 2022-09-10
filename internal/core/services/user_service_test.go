3bcf0a7e6cd3ba543fca89569181b4e621716ff0
Gabriel Magalhães <magalhaes.gabrielf@gmail.com>
2022-09-10 20:00:52 -0300

    ensure that user can be registered

---
+++ internal/core/services/user_service_test.go
@@ -0,0 +1,31 @@
+package services
+
+import (
+	"login-app/internal/api/web/user/dto"
+	"login-app/internal/core/domain"
+	"login-app/platform/logger"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type MockUserRepository struct {
+}
+
+func (r MockUserRepository) Insert(user *domain.User) (string, error) {
+	return "id", nil
+}
+
+func TestGivenUserRequest_whenNewUser_thenReturnObjectId(t *testing.T) {
+	s := NewService(logger.LoggerMock{}, MockUserRepository{})
+
+	id, err := s.Create(&dto.CreateUserRequest{
+		Name:                 "john doe",
+		Email:                "johndoe@email.com",
+		Password:             "12345678",
+		PasswordConfirmation: "12345678",
+	})
+
+	assert.Equal(t, "eoq", id)
+	assert.Nil(t, err)
+}
